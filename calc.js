dccace55e1f1e2be687e1ad13a3ca9802582de64
zentestuken <88427701+zentestuken@users.noreply.github.com>
2021-10-05 23:07:00 +0300

    Added ability to operate multidigital integers and fractional numbers

--- calc.js
+++ calc.js
@@ -4,16 +4,16 @@ let webdriver = require('selenium-webdriver'),
 
 let driver = new webdriver.Builder().forBrowser('chrome').build();
 
-
-// testing online calculator's basic arithmetic operations (+, -, *, /)
-// first and second numbers are generated as integers (1 to 9)
-// test cycles through all four basic operations
+// testing online calculator's basic arithmetic operations
+// first and second numbers are generated (can be fractional)
+// test cycles through all 4 basic operations
+// NOTE: calculator output displays only 12 digits (not including sign)
 (async function testRun() {
     let ops = ['+', '-', '*', '/'];
     let result = 'N/A';
     for (let i = 0; i < 4; i++) {
-        let first = Math.floor((Math.random() * 9) + 1);
-        let second = Math.floor((Math.random() * 9) + 1);
+        let first = (Math.random() * 10000) + 1;
+        let second = (Math.random() * 10000) + 1;
         let operation = ops[i];
         switch (operation) {
             case '+': result = first + second; break;
@@ -21,22 +21,47 @@ let driver = new webdriver.Builder().forBrowser('chrome').build();
             case '*': result = first * second; break;
             case '/': result = first / second; break;
             default: throw 'Error: no operation set for testing';}
-        result = Math.round((result + Number.EPSILON) * 10000000000) / 10000000000;
+        let dotPos = result.toString().indexOf('.');
+        // this rounds the result so that it's length to be <= 12 (not including sign)
+        switch (result.toString()[0]) {
+            case '-':
+                result = Math.round((result + Number.EPSILON)
+                    * Math.pow(10, (13 - dotPos - 1))) / Math.pow(10, (13 - dotPos - 1)); break;
+            default:
+                result = Math.round((result + Number.EPSILON)
+                    * Math.pow(10, (12 - dotPos - 1))) / Math.pow(10, (12 - dotPos - 1));
+        }
         await basicOps(first, second, operation);
+
     }
 
     async function basicOps(f, s, op) {
         driver.get('https://www.calculator.net/');
-        driver.findElement(By.xpath("//span[@onclick='r(" + f + ")']"))
-            .click();
+        let fStr = f.toString();
+        let sStr = s.toString();
+        for (const el of fStr) {
+            switch (el) {
+                case '.':
+                    await driver.findElement(By.xpath("//span[@onclick=\"r('.')\"]"))
+                        .click(); break;
+                default:
+                    await driver.findElement(By.xpath("//span[@onclick='r(" + el + ")']"))
+                        .click();
+            } }
         driver.findElement(By.xpath("//span[@onclick=\"r('" + op + "')\"]"))
             .click();
-        driver.findElement(By.xpath("//span[@onclick='r(" + s + ")']"))
-            .click();
-        driver.findElement(By.xpath("//span[@onclick=\"r('=')\"]"))
+        for (const el of sStr) {
+            switch (el) {
+                case '.':
+                    await driver.findElement(By.xpath("//span[@onclick=\"r('.')\"]"))
+                        .click(); break;
+                default:
+                    await driver.findElement(By.xpath("//span[@onclick='r(" + el + ")']"))
+                        .click();
+            } }
+        await driver.findElement(By.xpath("//span[@onclick=\"r('=')\"]"))
             .click();
         let output = driver.findElement(By.css("div#sciOutPut"));
-
         await driver.wait(until.elementTextIs(output, ' ' + result), 5000);
         await output.getText().then((txt) => {
             console.log(f + ' ' + op + ' ' + s + ' = ' + txt + '  | correct');
@@ -45,5 +70,3 @@ let driver = new webdriver.Builder().forBrowser('chrome').build();
 
 
 })();
-
-//LOLOLOLOL
