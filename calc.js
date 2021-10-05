65dde2385db06fb4517317ec89753f0d76e2f282
zentestuken <88427701+zentestuken@users.noreply.github.com>
2021-10-05 22:04:40 +0300

    Expanded the commentary

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ let webdriver = require('selenium-webdriver'),
 
 let driver = new webdriver.Builder().forBrowser('chrome').build();
 
-// testing online calculator's basic arithmetic operations
+// testing online calculator's basic arithmetic operations (+, -, *, /)
 // first and second numbers are generated as integers (1 to 9)
 // test cycles through all four basic operations
 (async function testRun() {
